0aa19504f573842b484a1e186f46119219ded911
Ashish Raj <rajashish7370021080@gmail.com>
2026-05-12 10:39:46 +0530

    Update StudentDAO.java

--- src/src/StudentDAO.java
+++ src/src/StudentDAO.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 
 public class StudentDAO {
 
+    // Add Student
     public void addStudent(Student student) {
 
         String query = "INSERT INTO students(name, course, age) VALUES (?, ?, ?)";
@@ -20,6 +21,33 @@ public class StudentDAO {
             preparedStatement.executeUpdate();
 
             System.out.println("Student Added Successfully");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // View Students
+    public void viewStudents() {
+
+        String query = "SELECT * FROM students";
+
+        try {
+            Connection connection = DatabaseConnection.getConnection();
+
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+
+            var resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+
+                System.out.println("ID: " + resultSet.getInt("id"));
+                System.out.println("Name: " + resultSet.getString("name"));
+                System.out.println("Course: " + resultSet.getString("course"));
+                System.out.println("Age: " + resultSet.getInt("age"));
+
+                System.out.println("----------------------");
+            }
 
         } catch (SQLException e) {
             e.printStackTrace();
